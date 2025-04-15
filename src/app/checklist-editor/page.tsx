8acc5b5eaa51ddd685a9b8c0599a72d4763493c1
mdvertola <mdvertola@gmail.com>
2025-04-15 15:35:05 -0400

    fixing build

--- src/app/checklist-editor/page.tsx
+++ src/app/checklist-editor/page.tsx
@@ -1,6 +1,4 @@
 import { XmlViewer } from "@/components/XmlViewer";
-import Link from "next/link";
-import Image from "next/image";
 import Nav from "@/components/nav";
 export default function Home() {
     return (
